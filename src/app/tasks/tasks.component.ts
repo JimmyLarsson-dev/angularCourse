717ec4dcbc1713afaff239b76d3478a4280be6c7
Jimmy Larsson <jimmy.larsson@europe.com>
2024-08-22 20:24:07 +0200

    at part2.55

--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -1,17 +1,21 @@
 import { Component, Input, output, Output, EventEmitter } from '@angular/core';
 import { TaskComponent } from './task/task.component';
-
+import { AddTaskComponent } from '../add-task/add-task.component'
 
 @Component({
     selector: 'app-tasks',
     standalone: true,
-    imports: [TaskComponent],
+    imports: [TaskComponent, AddTaskComponent],
     templateUrl: './tasks.component.html',
     styleUrl: './tasks.component.css'
 })
 export class TasksComponent {
-    @Input({required: true}) userId!: string;
-    @Input({required: true}) name!: string | undefined;
+  
+  taskCreatingInProgress = false;  
+  @Input({required: true}) userId!: string;
+  @Input({required: true}) name!: string | undefined;
+    
+
     tasks = [
         {
             id: 't1',
@@ -36,12 +40,21 @@ export class TasksComponent {
           dueDate: '2024-06-15',
         },
     ];
+
     get selectedUserTasks() {
         return this.tasks.filter((task) => task.userId === this.userId)
     }
 
     onCompleteTask(id: string) {
       this.tasks = this.tasks.filter((task) => task.id !== id);
+    }
+
+    onClickedAddTask() {
+      this.taskCreatingInProgress = true;
+    }
+
+    onCancelAddTask() {
+      this.taskCreatingInProgress = false;
     }
 
 }
